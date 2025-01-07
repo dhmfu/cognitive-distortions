4ea98351551b873df0a491a885019eb9e36eb764
Vasyl Dudka <dudka.vasia2010@gmail.com>
2025-01-07 23:48:55 +0200

    Feature: App Updates (#3)
    * rough toast of updates
    
    * toast with actions
    
    * un-hardcode app version
    
    * add version script to build processes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,9 @@
-import { Component, HostListener, inject } from '@angular/core';
-import { MatSnackBar, MatSnackBarConfig, MatSnackBarModule } from '@angular/material/snack-bar';
-import { RouterOutlet } from '@angular/router';
-import 'hammerjs';
-import { AppToolbarComponent } from "./components/app-toolbar/app-toolbar.component";
-
+import { Component } from '@angular/core';
+import { CoreComponent } from './components/core/core.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppToolbarComponent, MatSnackBarModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CoreComponent],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  private snackBar = inject(MatSnackBar);
-
-  @HostListener('press', ['$event']) onPress(pressEvent: HammerInput) {
-    pressEvent.preventDefault();
-    pressEvent.srcEvent.preventDefault();
-    pressEvent.srcEvent.stopImmediatePropagation();
-    const snackOptions: MatSnackBarConfig = {
-      duration: 500,
-      panelClass: ['text-lg', 'no-select']
-    }
-    this.snackBar.open(`version: 0.0.1`, '', snackOptions);
-
-  }
-}
+export class AppComponent { }
---
+++ src/app/components/core/core.component.ts
@@ -0,0 +1,60 @@
+import { Component, DestroyRef, HostListener, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterOutlet } from '@angular/router';
+import { filter, switchMap } from 'rxjs/operators';
+import { AppUpdateService } from '../../services/app-update.service';
+import { APP_VERSION } from '../../tokens/version.token';
+import { AppToolbarComponent } from '../app-toolbar/app-toolbar.component';
+import { UpdateToastComponent } from '../update-toast/update-toast.component';
+
+@Component({
+  selector: 'core',
+  imports: [RouterOutlet, AppToolbarComponent, MatSnackBarModule],
+  templateUrl: './core.component.html',
+  styleUrl: './core.component.scss'
+})
+export class CoreComponent implements OnInit {
+  private snackBar = inject(MatSnackBar);
+  private appUpdates = inject(AppUpdateService);
+  private destroyRef = inject(DestroyRef);
+  private appVersion = inject(APP_VERSION);
+
+  ngOnInit(): void {
+    this.pollAppUpdates();
+  }
+
+  @HostListener('press', ['$event']) onPress(pressEvent: HammerInput) {
+    pressEvent.preventDefault();
+    pressEvent.srcEvent.preventDefault();
+    pressEvent.srcEvent.stopImmediatePropagation();
+    
+    const snackOptions: MatSnackBarConfig = {
+      duration: 1500,
+      panelClass: ['text-lg', 'no-select']
+    };
+
+    this.snackBar.open(`version: ${this.appVersion}`, '', snackOptions);
+  }
+
+  private pollAppUpdates(): void {
+    this.appUpdates.updateAvailable$.pipe(
+      takeUntilDestroyed(this.destroyRef),
+      switchMap(event => {
+        const version = (event.latestVersion.appData as any)?.version as string;
+        const snackOptions: MatSnackBarConfig = {
+          duration: 5000,
+          panelClass: ['text-lg'],
+          data: version
+        };
+  
+        const toastRef = this.snackBar.openFromComponent(UpdateToastComponent, snackOptions);
+        
+        return toastRef.afterDismissed();
+      }),
+      filter(dismiss => dismiss.dismissedByAction)
+    ).subscribe(() => {
+      document.location.reload();
+    });
+  }
+}
