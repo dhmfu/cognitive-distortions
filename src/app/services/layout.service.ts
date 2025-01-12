50b9ed2ef5c6d9a5aeaa383f46efd81db25ea084
Vasyl Dudka <dudka.vasia2010@gmail.com>
2025-01-13 00:18:17 +0200

    Bugfix: layout save

--- src/app/services/layout.service.ts
+++ src/app/services/layout.service.ts
@@ -1,17 +1,29 @@
-import { computed, Injectable, Signal, signal } from '@angular/core';
+import { inject, Injectable, Signal, signal, WritableSignal } from '@angular/core';
 import { Layout } from '../constants/layout.enum';
+import { LocalStorageService } from './local-storage.service';
+import { LocalKey } from '../constants/local-keys.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LayoutService {
-  private current = signal(Layout.Rows);
+  private localStorage = inject(LocalStorageService);
+
+  private current = this.initCurrent();
 
   getCurrent(): Signal<Layout> {
-    return computed(() => this.current());
+    return this.current.asReadonly();
   }
 
   setCurrent(layout: Layout): void {
+    this.localStorage.set(LocalKey.Layout, layout);
+
     this.current.set(layout);
+  }
+
+  private initCurrent(): WritableSignal<Layout> {
+    const localLayout = this.localStorage.get<Layout>(LocalKey.Layout);
+
+    return signal(localLayout ?? Layout.Rows);
   }
 }
---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { LocalKey } from '../constants/local-keys.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  private storage = this.initStorage();
+
+  set(key: string, value: any): void {
+    const stringValue = JSON.stringify(value);
+
+    const updatedLocalMap = this.storage().set(key, stringValue);
+
+    this.storage.set(updatedLocalMap);
+
+    localStorage.setItem(key, stringValue);
+  }
+
+  get<T>(key: LocalKey): T | null {
+    const localValue = this.storage().get(key);
+    
+    return localValue && JSON.parse(localValue);
+  }
+
+  private initStorage(): WritableSignal<Map<string, string | null>> {
+    const localEntries = Object.values(LocalKey).reduce((entries, localKey) => {
+      const localValue = localStorage.getItem(localKey);
+      
+      const entry: [string, string | null] = [localKey, localValue];
+
+      return [...entries, entry];
+    }, [] as [string, string | null][]);
+
+    return signal(new Map<string, string | null>(localEntries));
+  }
+}
